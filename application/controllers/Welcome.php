3120fc1db60b6f5050d5f01b10d01396899d6720
Lukas <luszabo2000@gmail.com>
2018-05-01 11:17:54 +0200

    Upraveny vzhlad stranok

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,12 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+        /**
+         * $this->load->view('welcome_message');
+         *
+         */
+        $this->load->view('template/header');
+        $this->load->view('template/footer');
+
 	}
 }
