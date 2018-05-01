52ceb1660e4a210f2bc76de7d4622bdd00017c6b
Lukas <luszabo2000@gmail.com>
2018-05-01 12:12:50 +0200

    home...strana 9

---
+++ application/controllers/Home.php
@@ -0,0 +1,14 @@
+<?php
+defined('basepath') or exit('no direct script access allowed')
+
+class Home extends CI_Controller {
+    public function index()
+    {
+        $this->load->view('template/header');
+        $this->load->view('template/navigation');
+        $this->load->view('home');
+        $this->load->view('template/footer');
+
+
+    }
+}
